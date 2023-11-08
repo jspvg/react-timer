4e4c18a89a083001cafd6b45fc99fc9143b16443
jspvg <josipovic1501@gmail.com>
2023-11-08 22:49:29 +0100

    expanding polymorphic component with generics

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Container from "./components/Container";
 function App() {
   return (
     <main>
-      <Container as={Button} />
+      <Container as={Button} onClick={() => console.log("it works")}>
+        Click me
+      </Container>
     </main>
   );
 }
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,21 @@
-import { type ElementType } from "react";
+import {
+  type ElementType,
+  type ReactNode,
+  type ComponentPropsWithoutRef,
+} from "react";
 
-interface ContainerProps {
-  as: ElementType;
-}
+type ContainerProps<T extends ElementType> = {
+  as?: T;
+  children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
 
-const Container = ({ as }: ContainerProps) => {
-  const Container = as;
-  return <Container />;
+const Container = <C extends ElementType>({
+  as,
+  children,
+  ...props
+}: ContainerProps<C>) => {
+  const Component = as || "div";
+  return <Component {...props}>{children}</Component>;
 };
 
 export default Container;
