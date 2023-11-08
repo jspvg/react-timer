61e202167f109e781e08246c942cb8a827d4b612
jspvg <josipovic1501@gmail.com>
2023-11-08 19:41:41 +0100

    created input component scaffold

---
+++ src/components/Input.tsx
@@ -0,0 +1,15 @@
+interface InputProps {
+  label: string;
+  id: string;
+}
+
+const Input = ({ label, id }: InputProps) => {
+  return (
+    <p>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type="text" />
+    </p>
+  );
+};
+
+export default Input;
