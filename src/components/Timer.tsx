9956d057b1b439c30435700fc5f12668f043ce3e
jspvg <josipovic1501@gmail.com>
2023-11-10 18:47:45 +0100

    implemented start/stop functionality with useEffect and custom hook

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,46 @@
 import Container from "./UI/Container.tsx";
 import { type Timer as TimerProps } from "../types/timerTypes.ts";
+import { useEffect, useRef, useState } from "react";
+import useTimersContext from "../hooks/useTimersContext.ts";
 
-const Timer = ({ name, duration }: TimerProps) => (
-  <Container as="article">
-    <h2>{name}</h2>
-    <p>{duration}</p>
-  </Container>
-);
+const Timer = ({ name, duration }: TimerProps) => {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime;
+          }
+          return prevTime - 50;
+        });
+      }, 50);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  return (
+    <Container as="article">
+      <h2>{name}</h2>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
+    </Container>
+  );
+};
 
 export default Timer;
